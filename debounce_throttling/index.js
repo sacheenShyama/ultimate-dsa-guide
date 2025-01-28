d55c25567e39526efb63f9d00042637dc0babe26
sacheenShyama <nomadsachin2002@gmail.com>
2025-01-29 00:54:29 +0530

    debounce and throttling implemented

---
+++ debounce_throttling/index.js
@@ -0,0 +1,46 @@
+const searchInput = document.getElementById("searchInput");
+
+function debounce(func, delay) {
+  let timeout;
+  return function (...args) {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      func(...args);
+    }, delay);
+  };
+}
+
+const myFunction = function (event) {
+  const query = event.target.value;
+  console.log("query is:", query);
+
+  //    fetch(`https://api.example.com/search?query=${query}`)
+  //    .then((response)=> response.json())
+  //    .then((data)=>{
+  //       console.log(data)
+  //    });
+};
+
+searchInput.addEventListener("input", debounce(myFunction, 500));
+
+// Throttling
+
+const button = document.getElementById("clickButton");
+
+function throttle(func, delay) {
+  let lastCall = 0;
+
+  return function (...args) {
+    const now = Date.now();
+    if (now - lastCall >= delay) {
+      lastCall = now;
+      func(...args);
+    }
+  };
+}
+
+const buttonClick = () => {
+  console.log("button clicked...");
+};
+
+button.addEventListener("click", throttle(buttonClick, 500));
