c3e167871c86d211eeaafa8b560fca1a9a3af95e
Sachin kumar <93372997+sacheenShyama@users.noreply.github.com>
2025-03-01 23:43:04 +0530

    Update four.js

--- four.js
+++ four.js
@@ -5,13 +5,13 @@ for(var i=0; i<10; i++){
 }
 
 
-var obj1 = {
-    address : "Mumbai,India",
-    getAddress: function(){
-    console.log(this.address); 
-  }
-}
+// var obj1 = {
+//     address : "Mumbai,India",
+//     getAddress: function(){
+//     console.log(this.address); 
+//   }
+// }
    
-var getAddress = obj1.getAddress;
-var obj2 = {name:"akshay"};
-obj2.getAddress();  
+// var getAddress = obj1.getAddress;
+// var obj2 = {name:"akshay"};
+// obj2.getAddress();  
